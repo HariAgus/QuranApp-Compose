c1bed66ac5499e954dbd124b7c5a8f1e40a9ec75
HariAgus <haryagus97@gmail.com>
2022-05-02 02:57:53 +0700

    Setup dagger hilt

---
+++ app/src/main/java/com/haw/quranappcompose/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.haw.quranappcompose.di
+
+import com.haw.quranappcompose.data.remote.QuranApi
+import com.haw.quranappcompose.utils.Constants.BASE_URL
+import com.haw.quranappcompose.utils.Constants.MEDIA_TYPE
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get(MEDIA_TYPE)
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuranApi(retrofit: Retrofit): QuranApi {
+        return retrofit.create(QuranApi::class.java)
+    }
+
+}
